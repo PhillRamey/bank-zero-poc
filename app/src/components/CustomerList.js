a3165d1e053d566756db256cc83b24865c08f5ed
Phill Ramey <pramey@gmail.com>
2019-03-24 01:03:33 -0700

    Added permission-based customer list

---
+++ app/src/components/CustomerList.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import axios from 'axios';
+
+class CustomerList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      customers: [],
+      accessToken: props.accessToken
+    };
+  }
+
+  async componentDidMount() {
+    const customers = (await axios.get('http://localhost:3001/customers', { headers: { 'Authorization': `Bearer ${this.state.accessToken}`}})).data;
+    this.setState({
+      customers,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Customer List</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>User ID</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            this.state.customers.map(customer => (
+              <tr key={customer.user_id}>
+                <td>{customer.name}</td>
+                <td>{customer.email}</td>
+                <td>{customer.user_id}</td>
+              </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default CustomerList;
--- app/src/pages/dashboard.js
+++ app/src/pages/dashboard.js
@@ -3,24 +3,29 @@ import { Redirect } from "react-router-dom";
 
 import { AuthConsumer } from "../authContext";
 import CustomerPanel from "../components/CustomerPanel.js";
+import CustomerList from "../components/CustomerList.js";
 
 const DashboardPage = () => (
   <AuthConsumer>
     {({ authenticated, user, accessToken }) =>
-      authenticated ? (
-        <div className="container">
-            <h1>Dashboard<br/>
-              <small className="text-muted">Welcome to your Bank Zero account.</small>
-            </h1>
-            <CustomerPanel permissions={user.permissions} />
-            <p>{user.id}</p>
-            <p>{user.name}</p>
-            <p>{user.permissions}</p>
-            <p>{accessToken}</p>
-        </div>
-      ) : (
-        <Redirect to="/" />
-      )
+      !authenticated ? (<Redirect to="/" />) : 
+        user.permissions.includes("read:customers") ?
+          (
+            <div className="container">
+              <h1>Dashboard<br/>
+                <small className="text-muted">Welcome to your Bank Zero account.</small>
+              </h1>
+              <CustomerList permissions={user.permissions} accessToken={accessToken} />
+            </div>
+          ) :
+          (
+            <div className="container">
+              <h1>Dashboard<br/>
+                <small className="text-muted">Welcome to your Bank Zero account.</small>
+              </h1>
+              <CustomerPanel permissions={user.permissions} />
+            </div>
+          )
     }
   </AuthConsumer>
 );
