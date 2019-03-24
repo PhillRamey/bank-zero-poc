f305c5452d76d074d7b70011e8c76f4d42031755
Phill Ramey <pramey@gmail.com>
2019-03-23 20:58:42 -0700

    jwt validation and endpoint to return permissions from access token

--- api/src/index.js
+++ api/src/index.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+require('dotenv').config();
+
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'
+}
 
 // define the Express app
 const app = express();
@@ -26,6 +34,24 @@ app.use(morgan('combined'));
 //placeholder route
 app.get('/', (req, res) => {
   res.send({})
+});
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+app.get('/permissions', checkJwt, (req, res) => {
+  res.send(req.user.permissions);
 });
 
 // start the server
